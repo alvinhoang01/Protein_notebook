f71355cafe9be3abd2c3128537c485f02c54f18a
alvinhoang01 <141177556+alvinhoang01@users.noreply.github.com>
2025-03-29 20:54:55 -0400

    Image updated

--- Home_page.py
+++ Home_page.py
@@ -8,19 +8,28 @@ def main():
             .title {
                 font-family: "Arial", sans-serif;
                 color: #008080;
-                font-size: 42px;
+                font-size: 50px;
                 font-weight: bold;
             }
             .subtitle {
                 font-family: "Arial", sans-serif;
                 color: #800000;
-                font-size: 22px;
+                font-size: 35px;
             }
             .main-header {
                 font-family: "Arial", sans-serif; 
-                font-size: 28px;
+                font-size: 35px;
                 font-weight: bold;
                 text-decoration: underline;
+            }
+            .description {
+                font-family: "Arial", sans-serif;
+                font-size: 28px;  
+                line-height: 1.6;
+            }
+            .description li {
+                font-size: 25px;
+                margin-bottom: 8px;
             }
         </style>
         """,
@@ -31,14 +40,29 @@ def main():
     st.write("\n")
 
     # Display the static image (logo or decorative image)
-    data_path = os.path.join('data', "Protein_logo.png")
-    st.image(data_path, use_column_width=True)
+    from PIL import Image
 
-    st.write("""
-    Our web platform enables researchers to access mass spectrometry-based proteomics data on protein detectability, signal intensity, and quantification reproducibility across various sample preparation methods. Explore features including:
-    - Comparative performance analysis of blood proteins across different sample preparation techniques
-    - Comprehensive performance analysis of proteins in different clinical samples
-    """)
+    data_path = os.path.join('data', "Workflow_V4.tif")
+    image = Image.open(data_path)
+
+    # Create three columns and put the image in the center one
+    col1, col2, col3 = st.columns([1, 2, 2])
+    with col2:
+        st.image(image, width=1000)
+
+
+    st.markdown(
+    """
+    <div class='description'>
+        Our web platform enables researchers to access mass spectrometry-based proteomics data on protein detectability, signal intensity, and quantification reproducibility across various sample preparation methods. Explore features including:
+        <ul>
+            <li>Comparative performance analysis of blood proteins across different sample preparation techniques</li>
+            <li>Comprehensive performance analysis of proteins in different clinical samples</li>
+        </ul>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
     # Example of an exit button
     left_column2, right_column2 = st.columns([18, 2])
